2979eb50499b9965beac91de19608f10768e56f7
Andrys Salazar <sirzan125@gmail.com>
2021-09-27 17:54:45 -0500

    ajuste de la fecha america latina

--- api/remesasenviadas.api.php
+++ api/remesasenviadas.api.php
@@ -1,8 +1,14 @@
 <?php
 require_once '../models/conexion.php';
+date_default_timezone_set('America/Lima');
 
+$fecha = date('Y-m-d');
+$hora = date('H:i:s');
+
+$fechaActual = $fecha.' '.$hora;
 // Obtener registros
-$stmt = Conexion::conectar()->prepare("SELECT SUM(total_remesa) AS total,iso_tasa,simbolo_tasa FROM remesas WHERE DATE_FORMAT(created_at, '%Y-%m-%d') = CURDATE()  GROUP BY iso_tasa ");
+$stmt = Conexion::conectar()->prepare("SELECT SUM(total_remesa) AS total,iso_tasa,simbolo_tasa FROM remesas WHERE DATE_FORMAT(fecha, '%Y-%m-%d') = DATE_FORMAT(:fecha, '%Y-%m-%d')  GROUP BY iso_tasa ");
+$stmt -> bindParam(":fecha", $fechaActual, PDO::PARAM_STR);
 $stmt->execute();
 $remesa = $stmt->fetchAll();
 
