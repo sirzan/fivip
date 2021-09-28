adfe453151dd4d31e5ba6b7d590613fc42160a36
Andrys Salazar <sirzan125@gmail.com>
2021-09-28 13:02:32 -0500

    fecha de reportes ajustadas

--- models/reporte-dia.api.php
+++ models/reporte-dia.api.php
@@ -5,7 +5,7 @@ class ReporteDia{
 
     static public function apiReporte(){
 
-        $stmt = Conexion::conectar()->prepare("SELECT id,correlativo,pais,nombre_moneda,iso_moneda,simbolo_moneda,total_envio,tasa,total_remesa,iso_tasa,simbolo_tasa FROM remesas WHERE DATE_FORMAT(remesas.created_at, '%Y-%m-%d') = CURDATE()");
+        $stmt = Conexion::conectar()->prepare("SELECT id,correlativo,pais,nombre_moneda,iso_moneda,simbolo_moneda,total_envio,tasa,total_remesa,iso_tasa,simbolo_tasa FROM remesas WHERE DATE_FORMAT(remesas.fecha, '%Y-%m-%d') = CURDATE()");
          
         $stmt -> execute();
         return $stmt -> fetchAll();
@@ -16,7 +16,7 @@ class ReporteDia{
     }
     static public function apiReportemontoTotales(){
 
-        $stmt = Conexion::conectar()->prepare("SELECT SUM(total_envio)AS total,iso_moneda,simbolo_moneda FROM remesas WHERE DATE_FORMAT(remesas.created_at, '%Y-%m-%d') = CURDATE() GROUP BY iso_moneda");
+        $stmt = Conexion::conectar()->prepare("SELECT SUM(total_envio)AS total,iso_moneda,simbolo_moneda FROM remesas WHERE DATE_FORMAT(remesas.fecha, '%Y-%m-%d') = CURDATE() GROUP BY iso_moneda");
          
         $stmt -> execute();
         return $stmt -> fetchAll();
@@ -27,7 +27,7 @@ class ReporteDia{
     }
     static public function apiReporteremesaTotales(){
 
-        $stmt = Conexion::conectar()->prepare("SELECT SUM(total_remesa) AS total,iso_tasa,simbolo_tasa FROM remesas WHERE DATE_FORMAT(created_at, '%Y-%m-%d') = CURDATE()  GROUP BY iso_tasa");
+        $stmt = Conexion::conectar()->prepare("SELECT SUM(total_remesa) AS total,iso_tasa,simbolo_tasa FROM remesas WHERE DATE_FORMAT(fecha, '%Y-%m-%d') = CURDATE()  GROUP BY iso_tasa");
          
         $stmt -> execute();
         return $stmt -> fetchAll();
